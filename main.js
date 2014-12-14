1044c4d2eca0ed0b69d20f2f7aa781ada49f8846
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-12-13 17:05:57 -0800

    Add basic d3 viz to sound

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ var audio = T("audio").load("/drumkit.wav", function() {
     [SD],
   ].wrapExtend(128);
 
-  console.log(P1);
   var P2 = sc.series(16);
 
   var drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD, SD, HH1, HH2, CYM).play();
@@ -27,6 +26,9 @@ var audio = T("audio").load("/drumkit.wav", function() {
   var vcf  = T("MoogFF", {freq:2400, gain:6, mul:0.1}, lead);
   var env  = T("perc", {r:100});
   var arp  = T("OscGen", {wave:"sin(15)", env:env, mul:0.5});
+
+  console.log('p1 ', P1); 
+  console.log('p2 ', P2); 
 
   T("delay", {time:"BPM128 L4", fb:0.65, mix:0.35},
     T("pan", {pos:0.2}, vcf),
@@ -50,6 +52,7 @@ var audio = T("audio").load("/drumkit.wav", function() {
       lead.freq.linTo(noteNum.midicps() * 2, "100ms");
     }
     arp.noteOn(noteNum + 24, 60);
+    update(P1, i); 
   });
 });
 
@@ -64,3 +67,54 @@ function buttonPressed(){
   }
 
 }
+var width = 128 * 11;
+var height = 128 * 1; 
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    
+
+    
+function update(data, currentBeat) {
+
+  // DATA JOIN
+  // Join new data with old elements, if any.
+  var text = svg.selectAll("rect")
+      .data(data);
+
+  // // UPDATE
+  // // Update old elements as needed.
+  text.attr("fill", function(d, i) { 
+    if (i === currentBeat) 
+      return 'red';  
+    else
+      return 'blue'; 
+  });
+
+
+
+  // ENTER
+  // Create new elements as needed.
+  text.enter().append("rect")
+      .attr("class", "enter")
+      .attr("dy", ".35em")
+      .attr("y", 0)
+      .attr("x", function(d, i) { return i * 11; })
+      .attr('width', 10)
+      .attr('height', 10)
+      .attr('fill', 'red')
+      .style("fill-opacity", 1)
+      .text(function(d) { return d; })
+
+  // EXIT
+  // Remove old elements as needed.
+  text.exit()
+      .attr("class", "exit")
+    .transition()
+      .duration(750)
+      .attr("y", 60)
+      .style("fill-opacity", 1e-6)
+      .remove();
+}
+
